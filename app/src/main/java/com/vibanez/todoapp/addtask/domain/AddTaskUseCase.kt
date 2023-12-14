236b2ad9347f809a64c26ba7c7b10fe209481d1d
victor <vibanez18@gmail.com>
2023-12-14 08:08:35 +0100

    add use cases

---
+++ app/src/main/java/com/vibanez/todoapp/addtask/domain/AddTaskUseCase.kt
@@ -0,0 +1,10 @@
+package com.vibanez.todoapp.addtask.domain
+
+import com.vibanez.todoapp.addtask.data.TaskRepository
+import com.vibanez.todoapp.addtask.ui.model.TaskModel
+import javax.inject.Inject
+
+class AddTaskUseCase @Inject constructor(private val taskRepository: TaskRepository) {
+
+    suspend operator fun invoke(taskModel: TaskModel) = taskRepository.add(taskModel)
+}
---
+++ app/src/main/java/com/vibanez/todoapp/addtask/domain/GetTaskUseCase.kt
@@ -0,0 +1,10 @@
+package com.vibanez.todoapp.addtask.domain
+
+import com.vibanez.todoapp.addtask.data.TaskRepository
+import com.vibanez.todoapp.addtask.ui.model.TaskModel
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class GetTaskUseCase @Inject constructor(private val taskRepository: TaskRepository) {
+    operator fun invoke(): Flow<List<TaskModel>> = taskRepository.findAll()
+}
